0266b137078898be31d176f6c06c35d1449cdd84
Mohammed-Ghayaz <mghayaz04@gmail.com>
2025-12-25 15:35:10 +0530

    Document Ingestion logic implemented

--- backend/src/api/routes/upload.py
+++ backend/src/api/routes/upload.py
@@ -1,18 +1,35 @@
-from fastapi import APIRouter, UploadFile, File, Depends
+from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
 from typing import List
 from ...db.repository import create_conversation
 from ...db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
+from ...services.ingestion_service import ingest_documents
 
 router = APIRouter()
 
 @router.post("/upload")
-async def upload_documents(db: AsyncSession = Depends(get_db), files: List[UploadFile] = File(...)):
-    for file in files:
-        print(file.filename)
+async def upload_documents(db: AsyncSession = Depends(get_db), uploaded_files: List[UploadFile] = File(...)):
+    try:
+        files = []
 
-    conversation_id = await create_conversation(db=db, user_id="8e5af5a4-96ad-4eb0-9e4e-f019bd2c9c18")
+        if not uploaded_files or len(uploaded_files) == 0:
+            raise HTTPException(status_code=400, detail="Upload documents")
 
-    return {
-        "conversation_id": conversation_id
-    }
+        for file in uploaded_files:
+            content = await file.read()
+            text = content.decode("utf-8")
+            if len(text.strip()) == 0:
+                continue
+
+            files.append({"filename": file.filename, "text": text})
+
+        conversation_id = await create_conversation(db=db, user_id="8e5af5a4-96ad-4eb0-9e4e-f019bd2c9c18")
+
+        await ingest_documents(conversation_id=conversation_id, files=files)
+
+        return {
+            "conversation_id": str(conversation_id)
+        }
+    
+    except Exception:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
