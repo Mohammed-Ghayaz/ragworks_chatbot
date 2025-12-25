add422ab1c403afa7a1b78b5f50afd07a07ce8f5
Mohammed-Ghayaz <mghayaz04@gmail.com>
2025-12-25 20:14:47 +0530

    Added routers to the fastapi app

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,14 @@ import logging
 import time
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from .src.api.routes import auth, chat, upload
 
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # restrict later in prod
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,24 +17,37 @@ app.add_middleware(
 
 http_logger = logging.getLogger("ragworks.http")
 
+
 @app.middleware("http")
 async def log_request_time(request: Request, call_next):
     start = time.perf_counter()
+    status = 500
+    level = logging.INFO
 
     try:
         response = await call_next(request)
-        level = logging.INFO
         status = response.status_code
+        level = logging.INFO
 
     except Exception:
-        status=500
         level = logging.ERROR
-        duration = (time.perf_counter() - start) * 1000
-        http_logger.exception(f"Unhandled exception in {request.method} {request.url.path}")
+        http_logger.exception(
+            f"Unhandled exception in {request.method} {request.url.path}"
+        )
         raise
 
     finally:
         duration = (time.perf_counter() - start) * 1000
-        http_logger.log(level, f"Request: {request.method} {request.url.path} | Status: {status} | Time taken: {duration} ms")
+        http_logger.log(
+            level,
+            f"Request: {request.method} {request.url.path} | "
+            f"Status: {status} | Time taken: {duration:.2f} ms"
+        )
 
     return response
+
+
+# API Routes
+app.include_router(auth.router)
+app.include_router(chat.router)
+app.include_router(upload.router)
